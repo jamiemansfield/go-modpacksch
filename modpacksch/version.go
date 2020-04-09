fdb568b574adb511c1b5a50c1620ae7b1c4c66b5
Jamie Mansfield <jmansfield@cadixdev.org>
2020-04-09 12:19:00 +0100

    Implement Version.Targets and Version.Files
    Signed-off-by: Jamie Mansfield <jmansfield@cadixdev.org>

--- modpacksch/version.go
+++ modpacksch/version.go
@@ -11,6 +11,31 @@ type Version struct {
 	Refreshed int64 `json:"refreshed"`
 
 	Specs *Specs `json:"specs"`
+	Targets []*Target `json:"targets"`
+	Files []*File `json:"files"`
+}
+
+type Target struct {
+	ID int `json:"id"`
+	Type string `json:"type"`
+	Name string `json:"name"`
+	Version string `json:"version"`
+	Updated int64 `json:"updated"`
+}
+
+type File struct {
+	ID int `json:"id"`
+	Type string `json:"type"`
+	Path string `json:"path"`
+	Name string `json:"name"`
+	Version string `json:"version"`
+	URL string `json:"url"`
+	Sha1 string `json:"sha1"`
+	Size int `json:"size"`
+	ClientOnly bool `json:"clientonly"`
+	ServerOnly bool `json:"serveronly"`
+	Optional bool `json:"optional"`
+	Updated int64 `json:"updated"`
 }
 
 type VersionChangelog struct {
