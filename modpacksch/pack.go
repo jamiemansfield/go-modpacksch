7331a1d41b90a64b6074727793cb23ce91ce4a0c
Jamie Mansfield <jmansfield@cadixdev.org>
2020-04-09 13:25:25 +0100

    Expose errors from the Pack service
    Signed-off-by: Jamie Mansfield <jmansfield@cadixdev.org>

--- modpacksch/pack.go
+++ modpacksch/pack.go
@@ -8,48 +8,48 @@ import "strconv"
 // modpacks.ch API docs: https://modpacksch.docs.apiary.io/#/reference/0/modpacks
 type PackService service
 
-func (s *PackService) GetPack(id int) *Pack {
+func (s *PackService) GetPack(id int) (*Pack, error) {
 	request, err := s.client.NewRequest("GET", "public/modpack/" + strconv.Itoa(id), nil)
 	if err != nil {
-		return nil
+		return nil, err
 	}
 
 	var response Pack
 	_, err = s.client.Do(request, &response)
-	return &response
+	return &response, err
 }
 
-func (s *PackService) GetVersion(packId int, versionId int) *Version {
+func (s *PackService) GetVersion(packId int, versionId int) (*Version, error) {
 	request, err := s.client.NewRequest("GET", "public/modpack/" + strconv.Itoa(packId) + "/" + strconv.Itoa(versionId), nil)
 	if err != nil {
-		return nil
+		return nil, err
 	}
 
 	var response Version
 	_, err = s.client.Do(request, &response)
-	return &response
+	return &response, err
 }
 
-func (s *PackService) GetVersionChangelog(packId int, versionId int) *VersionChangelog {
+func (s *PackService) GetVersionChangelog(packId int, versionId int) (*VersionChangelog, error) {
 	request, err := s.client.NewRequest("GET", "public/modpack/" + strconv.Itoa(packId) + "/" + strconv.Itoa(versionId) + "/changelog", nil)
 	if err != nil {
-		return nil
+		return nil, err
 	}
 
 	var response VersionChangelog
 	_, err = s.client.Do(request, &response)
-	return &response
+	return &response, err
 }
 
-func (s *PackService) Search(limit int, term string) []int {
+func (s *PackService) Search(limit int, term string) ([]int, error) {
 	request, err := s.client.NewRequest("GET", "public/modpack/search/" + strconv.Itoa(limit) + "?term=" + term, nil)
 	if err != nil {
-		return nil
+		return nil, err
 	}
 
 	var response searchResponse
 	_, err = s.client.Do(request, &response)
-	return response.Packs
+	return response.Packs, err
 }
 
 type searchResponse struct {
